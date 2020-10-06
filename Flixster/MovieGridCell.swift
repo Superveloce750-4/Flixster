4925c66a2a3b4e4fcf999e0a03a54cefdf7bfa1c
Pranav Rakkappan <59074909+Superveloce750-4@users.noreply.github.com>
2020-10-05 21:25:23 -0400

    Flix Part 2
    Added a more information section with a poster, image, and description. We also added a superhero movies section.

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Pranav Rakkappan on 10/5/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+}
